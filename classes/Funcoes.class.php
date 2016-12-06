142c3a9747d6400edef126f75f769ea9fa8a8e50
Escola WWW <contato@escolawww.com.br>
2016-12-06 13:49:37 -0200

    Revert "Revert "Formulário de cadastro em PHP Orientada a Objeto e PDO com MySQL""
    This reverts commit 908a7a7647bce8946dc5c05e56d4f53aab0732ca.

---
+++ classes/Funcoes.class.php
@@ -0,0 +1,32 @@
+<?php
+
+class Funcoes {
+    
+    public function tratarCaracter($vlr, $tipo){
+	switch($tipo){
+            case 1: $rst = utf8_decode($vlr); break;
+            case 2: $rst = htmlentities($vlr, ENT_QUOTES, "ISO-8859-1"); break;    
+        }
+        return $rst;
+    }	
+    
+    public function dataAtual($tipo){
+	switch($tipo){
+            case 1: $rst = date("Y-m-d"); break;
+            case 2: $rst = date("Y-m-d H:i:s"); break;
+            case 3: $rst = date("d/m/Y"); break;
+        }
+        return $rst;
+    }
+    
+    public function base64($vlr, $tipo){
+        switch($tipo){
+            case 1: $rst = base64_encode($vlr); break;
+            case 2: $rst = base64_decode($vlr); break;
+        }
+        return $rst;
+    }
+    
+}
+
+?>
